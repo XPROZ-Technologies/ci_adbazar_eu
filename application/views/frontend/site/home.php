62a20f939fd3dadc224053aeafdf2588d2a0dccd
Thienguyen <quyen.nguyen010398@gmail.com>
2021-09-11 13:51:17 +0700

    example map home

--- application/views/frontend/site/home.php
+++ application/views/frontend/site/home.php
@@ -6,6 +6,59 @@
     </select>
     <input type="hidden" name="UrlOld" value="<?php echo $this->uri->uri_string(); ?>"/>
     <input type="submit" hidden="hidden" name="" value=""/>
+
+    
 <?php echo form_close(); ?>
 <br>
 <span><?php echo $this->lang->line('hello'); ?><span>
+<br>
+<div class="col-sm-12 no-padding">
+    <div class="box box-default padding15">
+        <div id="mapLocation" style="height: 700px;width: 100%;"></div>
+    </div>
+</div>
+<?php 
+$configs = $this->Mconfigs->getListMap();
+?>
+<script>
+    var iconMarker = "<?php echo base_url(CONFIG_PATH.$configs['MARKER_MAP_IMAGE']) ?>";
+    var latMapAdmin = <?php echo LAT_MAP_ADMIN ?>;
+    var lngMapAdmin = <?php echo LNG_MAP_ADMIN ?>;
+    var zoomMapAdmin = <?php echo ZOOM_MAP_ADMIN ?>;
+    var locations = <?php echo json_encode($locations); ?>
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=<?php echo KEY_GOOGLE_MAP; ?>&callback=initMap&libraries=&v=weekly" async></script>
+<script>
+    console.log(locations)
+    function initMap() {
+    latitude = latMapAdmin;
+    longitude = lngMapAdmin;
+    
+    const myLatlng = { lat: parseFloat(latitude), lng: parseFloat(longitude) };
+   
+    
+    const map = new google.maps.Map(document.getElementById("mapLocation"), {
+        zoom: zoomMapAdmin,
+        center: myLatlng,
+    });
+
+    // Create the initial InfoWindow.
+    var infoWindow = new google.maps.InfoWindow();
+    let marker, i;
+    for (i = 0; i < locations.length; i++) {  
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i].lat, locations[i].lng),
+            map,
+            draggable:true,
+            animation: google.maps.Animation.DROP,
+            icon: iconMarker
+        });
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infoWindow.setContent(locations[i].location_name);
+                infoWindow.open(map, marker);
+            }
+        })(marker, i));
+    }
+}
+</script>
