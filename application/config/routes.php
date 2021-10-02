e93b6dd777475e328f7a2c4a646812226fa3fa00
manhm <haminhman2011@gmail.com>
2021-10-02 10:56:21 +0700

    up

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'backend/user';
+$route['default_controller'] = 'frontend/home';
 $route['404_override'] = '';//'user/permission';
 $route['translate_uri_dashes'] = FALSE;
 //===============================================================
--- application/views/frontend/includes/footer_login_signup.php
+++ application/views/frontend/includes/footer_login_signup.php
@@ -8,6 +8,10 @@
 <script src="assets/js/frontend/commons/ckeditor.js"></script>
 <script src="assets/js/frontend/commons/bootstrap.bundle.min.js"></script>
 <script src="assets/js/frontend/commons/main.js?version=<?php echo time(); ?>"></script>
+<script src="assets/js/frontend/common.js?version=<?php echo time(); ?>"></script>
+<script src="assets/vendor/plugins/pnotify/pnotify.custom.min.js"></script>
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+<meta name="google-signin-client_id" content="1001160309619-f30jgqido5nq8v2nt3gbdd0d7pr5hp7c.apps.googleusercontent.com">
 </body>
 
 </html>
