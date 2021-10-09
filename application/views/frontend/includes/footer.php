f947909bf67ac80f6cb7700f0e20fe461321d57d
levantru <letru.namdinh@gmail.com>
2021-10-09 18:09:57 +0700

    update datimepicker

--- application/views/frontend/includes/footer.php
+++ application/views/frontend/includes/footer.php
@@ -41,9 +41,12 @@
     <!--<script src="assets/js/frontend/commons/jquery.slim.min.js"></script>-->
     <script src="assets/js/frontend/commons/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.1/dist/umd/popper.min.js"></script>
+    
     <script src="assets/js/frontend/commons/owl.carousel.min.js"></script>
     <script src="assets/js/frontend/commons/moment.min.js"></script>
-    <script src="assets/js/frontend/commons/tempusdominus-bootstrap-4.min.js"></script>
+    <!-- <script src="assets/js/frontend/commons/tempusdominus-bootstrap-4.min.js"></script> -->
+    <script src="assets/js/frontend/commons/bootstrap.min.js"></script>
+    <script src="assets/js/frontend/commons/bootstrap-datetimepicker.js"></script>
     <script src="assets/js/frontend/commons/jquery.nice-select.min.js"></script>
     <script src="assets/js/frontend/commons/ckeditor.js"></script>
     <script src="assets/js/frontend/commons/bootstrap.bundle.min.js"></script>
