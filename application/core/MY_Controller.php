6ee84dd9520801d70482a4315ad77b4a53d865f3
quyen.nguyen010398 <quyen.nguyen010398@gmail.com>
2021-10-10 18:07:16 +0700

    up

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -223,4 +223,15 @@ abstract class MY_Controller extends CI_Controller {
         }
         return "";
     }
+
+    public function getLanguageFE() {
+        $this->load->helper('cookie');
+        $language = $this->input->cookie('customer') ? json_decode($this->input->cookie('customer', true), true)["language_name"] : config_item('language');
+        $this->language =  $language;
+        $this->lang->load('login', $this->language);
+        $this->lang->load('customer', $this->language);
+        $this->lang->load('business_profile', $this->language);
+        $this->lang->load('business_management', $this->language);
+        $this->lang->load('user_account_management', $this->language);
+    }
 }
