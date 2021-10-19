2ff807983095bd0ce91a2f6bdf201e9342c0a51c
Huong Thien <thien@Huongs-MacBook-Pro.local>
2021-10-19 11:08:51 +0700

    update bp-reser

--- application/views/frontend/business/bp-reservation.php
+++ application/views/frontend/business/bp-reservation.php
@@ -15,110 +15,112 @@
               <div class="bp-reservation">
                 <div class="bp-reservation-inner">
                   <div class="d-flex flex-column flex-lg-row align-items-lg-center justify-content-lg-between reservation-top">
-                  <?php $my_reservations_at_inspire_beauty_salon = $this->lang->line('my_reservations_at_inspire_beauty_salon'); 
-                $my_reservations_at_inspire_beauty_salon = explode('<Inspire Beauty Salon>', $my_reservations_at_inspire_beauty_salon);
-            ?>
+                    <?php $my_reservations_at_inspire_beauty_salon = $this->lang->line('my_reservations_at_inspire_beauty_salon');
+                    $my_reservations_at_inspire_beauty_salon = explode('<Inspire Beauty Salon>', $my_reservations_at_inspire_beauty_salon);
+                    ?>
                     <h5 class="page-title-xs mb-lg-0"><?php echo $my_reservations_at_inspire_beauty_salon[0]; ?> <?php echo $businessInfo['business_name']; ?><?php echo $my_reservations_at_inspire_beauty_salon[1]; ?></h5>
                     <?php if ($businessInfo['allow_book'] == STATUS_ACTIVED) { ?>
                       <a href="<?php echo base_url('business/' . $businessInfo['business_url'] . '/book-reservation'); ?>" class="btn btn-red"><?php echo $this->lang->line('book_a_reservation'); ?></a>
                     <?php } ?>
                   </div>
+
                   <div class="w-275">
-                  <div class="reservation-select-date">
-                    <div class="form-group form-group-datepicker">
-                      <label for="selecteDate" class="form-label"><?php echo $this->lang->line('select_a_date'); ?></label>
-                      <div class="datepicker-wraper position-relative">
-                        <img src="assets/img/frontend/icon-calendar.png" alt="calendar icon" class="img-fluid icon-calendar" />
-                        <input type="text" class="form-control datetimepicker-input" id="selecteDate" data-toggle="datetimepicker" value="" />
+                    <div class="reservation-select-date">
+                      <div class="form-group form-group-datepicker">
+                        <label for="selecteDate" class="form-label"><?php echo $this->lang->line('select_a_date'); ?></label>
+                        <div class="datepicker-wraper position-relative">
+                          <img src="assets/img/frontend/icon-calendar.png" alt="calendar icon" class="img-fluid icon-calendar" />
+                          <input type="text" class="form-control datetimepicker-input" id="selecteDate" data-toggle="datetimepicker" value="" />
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
+
                   <div class="bg-f5">
                     <?php if (!empty($lists)) { ?>
-                    <form class="d-flex search-box" action="<?php echo $basePagingUrl; ?>" method="GET" name="searchForm">
-                      <a href="javascript:void(0)" class="search-box-icon" onclick="document.searchForm.submit();"><img src="assets/img/frontend/ic-search.png" alt="search icon"></a>
-                      <input class="form-control" type="text" placeholder="Search" aria-label="Search" name="keyword" value="<?php echo $keyword; ?>">
-                    </form>
+                      <form class="d-flex search-box" action="<?php echo $basePagingUrl; ?>" method="GET" name="searchForm">
+                        <a href="javascript:void(0)" class="search-box-icon" onclick="document.searchForm.submit();"><img src="assets/img/frontend/ic-search.png" alt="search icon"></a>
+                        <input class="form-control" type="text" placeholder="Search" aria-label="Search" name="keyword" value="<?php echo $keyword; ?>">
+                      </form>
 
-                    <!-- Filter -->
-                    <div class="notification-wrapper-filter d-flex align-items-center justify-content-md-between">
-                      <div class="d-flex align-items-center inner-filter">
-                        
-                        <span class="me-2 page-text-lg fw-bold"><?php echo $this->lang->line('status'); ?></span>
-                        <div class="notification-filter">
-                          <div class="custom-select choose-reser-status">
+                      <!-- Filter -->
+                      <div class="notification-wrapper-filter d-flex align-items-center justify-content-md-between">
+                        <div class="d-flex align-items-center inner-filter">
+
+                          <span class="me-2 page-text-lg fw-bold"><?php echo $this->lang->line('status'); ?></span>
+                          <div class="notification-filter">
+                            <div class="custom-select choose-reser-status">
+                              <select>
+                                <option value="0"><?php echo $this->lang->line('1310_all'); ?></option>
+                                <option value="2" <?php if (isset($type) && $type == '2') {
+                                                    echo 'selected="selected"';
+                                                  } ?>><?php echo $this->lang->line('approved'); ?></option>
+                                <option value="3" <?php if (isset($type) && $type == '3') {
+                                                    echo 'selected="selected"';
+                                                  } ?>><?php echo $this->lang->line('declined'); ?></option>
+                                <option value="1" <?php if (isset($type) && $type == '1') {
+                                                    echo 'selected="selected"';
+                                                  } ?>><?php echo $this->lang->line('expired'); ?></option>
+                              </select>
+                            </div>
+                          </div>
+
+                        </div>
+                        <div class="d-flex align-items-center notification-sort">
+                          <img src="assets/img/frontend/ic-sort.png" alt="sort icon" class="img-fluid me-2">
+                          <div class="custom-select mb-0 choose-order">
                             <select>
-                              <option value="0"><?php echo $this->lang->line('1310_all'); ?></option>
-                              <option value="2" <?php if (isset($type) && $type == '2') {
-                                                  echo 'selected="selected"';
-                                                } ?>><?php echo $this->lang->line('approved'); ?></option>
-                              <option value="3" <?php if (isset($type) && $type == '3') {
-                                                  echo 'selected="selected"';
-                                                } ?>><?php echo $this->lang->line('declined'); ?></option>
-                              <option value="1" <?php if (isset($type) && $type == '1') {
-                                                  echo 'selected="selected"';
-                                                } ?>><?php echo $this->lang->line('expired'); ?></option>
+                              <option value="desc" selected><?php echo $this->lang->line('1310_newest'); ?></option>
+                              <option value="asc" <?php if (isset($order_by) && $order_by == 'asc') {
+                                                    echo 'selected="selected"';
+                                                  } ?>><?php echo $this->lang->line('1310_oldest'); ?></option>
                             </select>
                           </div>
                         </div>
-                        
                       </div>
-                      <div class="d-flex align-items-center notification-sort">
-                        <img src="assets/img/frontend/ic-sort.png" alt="sort icon" class="img-fluid me-2">
-                        <div class="custom-select mb-0 choose-order">
-                          <select>
-                            <option value="desc" selected><?php echo $this->lang->line('1310_newest'); ?></option>
-                            <option value="asc" <?php if (isset($order_by) && $order_by == 'asc') {
-                                                  echo 'selected="selected"';
-                                                } ?>><?php echo $this->lang->line('1310_oldest'); ?></option>
-                          </select>
-                        </div>
-                      </div>
-                    </div>
-                    <!-- END. Filter -->
+                      <!-- END. Filter -->
 
-                    <div class="table-responsive reservation-table">
-                      <table class="table page-text-lg">
-                        <thead>
-                          <tr>
-                            <th>Time</th>
-                            <th>ID</th>
-                            <th>People</th>
-                            <th><?php echo $this->lang->line('status'); ?></th>
-                            <th>Action</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          
-                          <?php if (!empty($lists)) {
-                            foreach ($lists as $itemBook) { ?>
-                           
-                              <tr>
-                                <td><?php echo $itemBook['date_arrived']; ?><br><?php echo $itemBook['time_arrived']; ?></td>
-                                
-                                <td><?php echo $itemBook['book_code']; ?></td>
-                                <td><?php echo $itemBook['number_of_people']; ?></td>
-                                <td>
-                                  <?php if ($itemBook['book_status_id'] == STATUS_ACTIVED) { ?>
-                                    <a href="<?php echo $basePagingUrl.'?type='.STATUS_ACTIVED; ?>"><span class="badge badge-approved"><?php echo $this->lang->line('approved'); ?></span></a>
-                                  <?php } ?>
-                                  <?php if ($itemBook['book_status_id'] == 1 || $itemBook['book_status_id'] == 4 || strtotime($itemBook['date_arrived'] . ' ' . $itemBook['time_arrived']) < strtotime(date('Y-m-d H:i'))) { ?>
-                                    <a href="<?php echo $basePagingUrl.'?type=1'; ?>"><span class="badge badge-expire"><?php echo $this->lang->line('expired'); ?></span></a>
-                                  <?php } ?>
-                                  <?php if ($itemBook['book_status_id'] == 3) { ?>
-                                    <a href="<?php echo $basePagingUrl.'?type=3'; ?>"><span class="badge badge-declined"><?php echo $this->lang->line('cancelled'); ?></span></a>
-                                  <?php } ?>
-                                </td>
-                                <td>
-                                  <div class="d-flex justify-content-center">
+                      <div class="table-responsive reservation-table">
+                        <table class="table page-text-lg">
+                          <thead>
+                            <tr>
+                              <th>Time</th>
+                              <th>ID</th>
+                              <th>People</th>
+                              <th><?php echo $this->lang->line('status'); ?></th>
+                              <th>Action</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+
+                            <?php if (!empty($lists)) {
+                              foreach ($lists as $itemBook) { ?>
+
+                                <tr>
+                                  <td><?php echo $itemBook['date_arrived']; ?><br><?php echo $itemBook['time_arrived']; ?></td>
+
+                                  <td><?php echo $itemBook['book_code']; ?></td>
+                                  <td><?php echo $itemBook['number_of_people']; ?></td>
+                                  <td>
                                     <?php if ($itemBook['book_status_id'] == STATUS_ACTIVED) { ?>
-                                      <button data-book="<?php echo $itemBook['id']; ?>" data-code="<?php echo $itemBook['book_code']; ?>" type="button" class="btn  btn-outline-red btn-outline-red-md fw-bold btn-ask-cancel-reservation" ><?php echo $this->lang->line('cancel'); ?></button>
-                                    <?php }else if ($itemBook['book_status_id'] == 4 || $itemBook['book_status_id'] == 1 || $itemBook['book_status_id'] == 3) { ?>
-                                      <button type="button" class="btn  btn-outline-red btn-outline-red-md btn-outline-red-disabled" disabled><?php echo $this->lang->line('cancel'); ?></button>
+                                      <a href="<?php echo $basePagingUrl . '?type=' . STATUS_ACTIVED; ?>"><span class="badge badge-approved"><?php echo $this->lang->line('approved'); ?></span></a>
+                                    <?php } ?>
+                                    <?php if ($itemBook['book_status_id'] == 1 || $itemBook['book_status_id'] == 4 || strtotime($itemBook['date_arrived'] . ' ' . $itemBook['time_arrived']) < strtotime(date('Y-m-d H:i'))) { ?>
+                                      <a href="<?php echo $basePagingUrl . '?type=1'; ?>"><span class="badge badge-expire"><?php echo $this->lang->line('expired'); ?></span></a>
+                                    <?php } ?>
+                                    <?php if ($itemBook['book_status_id'] == 3) { ?>
+                                      <a href="<?php echo $basePagingUrl . '?type=3'; ?>"><span class="badge badge-declined"><?php echo $this->lang->line('cancelled'); ?></span></a>
                                     <?php } ?>
                                   </td>
-                                  
+                                  <td>
+                                    <div class="d-flex justify-content-center">
+                                      <?php if ($itemBook['book_status_id'] == STATUS_ACTIVED) { ?>
+                                        <button data-book="<?php echo $itemBook['id']; ?>" data-code="<?php echo $itemBook['book_code']; ?>" type="button" class="btn  btn-outline-red btn-outline-red-md fw-bold btn-ask-cancel-reservation"><?php echo $this->lang->line('cancel'); ?></button>
+                                      <?php } else if ($itemBook['book_status_id'] == 4 || $itemBook['book_status_id'] == 1 || $itemBook['book_status_id'] == 3) { ?>
+                                        <button type="button" class="btn  btn-outline-red btn-outline-red-md btn-outline-red-disabled" disabled><?php echo $this->lang->line('cancel'); ?></button>
+                                      <?php } ?>
+                                  </td>
+
                                 </tr>
                             <?php }
                             } ?>
@@ -132,7 +134,7 @@
                         <div class="d-flex align-items-center flex-column flex-md-row justify-content-between page-pagination">
                           <div class="d-flex align-items-center pagination-left">
                             <p class="page-text-sm mb-0 me-3"><?php echo $this->lang->line('1310_showing'); ?> <span class="fw-500"><?php echo ($page - 1) * $perPage + 1; ?> – <?php echo ($page - 1) * $perPage + count($lists); ?></span> <?php echo $this->lang->line('1310_of'); ?> <span class="fw-500"><?php echo number_format($rowCount); ?></span>
-                            <?php echo $this->lang->line('1310_results'); ?></p>
+                              <?php echo $this->lang->line('1310_results'); ?></p>
                             <div class="page-text-sm mb-0 d-flex align-items-center">
                               <div class="custom-select choose-perpage">
                                 <select>
@@ -216,20 +218,20 @@
 <!-- Modal confirm remove -->
 <div class="modal fade" id="removeEventModal" tabindex="-1" aria-labelledby="removeEventModalLabel" aria-hidden="true">
 
-    <div class="modal-dialog modal-dialog-centered modal-medium">
-      <div class="modal-content">
-        <div class="modal-header border-bottom-0">
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+  <div class="modal-dialog modal-dialog-centered modal-medium">
+    <div class="modal-content">
+      <div class="modal-header border-bottom-0">
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p class="text-center page-text-lg">Are you sure want to cancel the reservation
+          "<b id="reservationCode"></b>"?
+        </p>
+        <input type="hidden" id="selectedBookId" value="" />
+        <div class="d-flex justify-content-center">
+          <a href="javascript:void(0)" class="btn btn-red btn-yes btn-cancel-reservation"><?php echo $this->lang->line('yes'); ?></a>
+          <a href="javascript:void(0)" class="btn btn-outline-red btn-cancel" data-bs-dismiss="modal">Cancel</a>
         </div>
-        <div class="modal-body">
-          <p class="text-center page-text-lg">Are you sure want to cancel the reservation
-            "<b id="reservationCode"></b>"?
-          </p>
-          <input type="hidden" id="selectedBookId" value="" />
-          <div class="d-flex justify-content-center">
-            <a href="javascript:void(0)" class="btn btn-red btn-yes btn-cancel-reservation"><?php echo $this->lang->line('yes'); ?></a>
-            <a href="javascript:void(0)" class="btn btn-outline-red btn-cancel" data-bs-dismiss="modal">Cancel</a>
-          </div>
 
       </div>
     </div>
@@ -260,6 +262,7 @@
       },
     });
   });
+  
   $(window).ready(() => {
     <?php
     $bookSuccess = $this->session->flashdata('book_success');
