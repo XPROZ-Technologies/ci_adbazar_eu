98e3f61393100f4086a07ca617d130cd7a1af341
quyen.nguyen <quyen.nguyen010398@gmail.com>
2021-11-17 14:19:07 +0700

    up

--- assets/js/frontend/login/login.js
+++ assets/js/frontend/login/login.js
@@ -135,8 +135,13 @@ function onLoad() {
   }
 
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    loginGG_FB(profile.rT, profile.XS, profile.GU, profile.St, 2)
+    var auth2 = gapi.auth2.getAuthInstance();
+    var profile = auth2.currentUser.get().getBasicProfile();
+    var email = profile.getEmail();
+    var id = profile.getId();
+    var customer_first_name = profile.getFamilyName();
+    var customer_last_name = profile.getGivenName();
+    loginGG_FB(id, customer_first_name, customer_last_name, email, 2)
     return false;
 }
 
