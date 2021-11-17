017a3e622269a396254e01b7b8232575c980f770
quyen.nguyen010398 <quyen.nguyen010398@gmail.com>
2021-11-17 15:46:30 +0700

    update

--- application/controllers/api/v1/Customer.php
+++ application/controllers/api/v1/Customer.php
@@ -207,6 +207,8 @@ class Customer extends MY_Controller {
                 $data['created_at'] = getCurentDateTime();
                 $data['created_by'] = 0;
                 $data['free_trial'] = STATUS_FREE_TRIAL;
+                $token_active = guidV4('reset-passs');
+                $data['token'] = $token_active;
                 if (intval($postData['login_type_id']) == 0) {
                     $flag = $this->Mcustomers->save($data);
                 } else {
@@ -216,15 +218,14 @@ class Customer extends MY_Controller {
                     $flag = $this->Mcustomers->save($data, $customerId);
                 }
                 if($flag) {
-                    $this->load->library('Authorization_Token');
                     // generte a token
-                    $token_active = $this->authorization_token->generateToken(array('id' => $flag, 'created_at' => getCurentDateTime()));
+                   
                     $this->load->model('Memailqueue');
                     $dataEmail = array(
                         'name' => $postData['customer_email'],
                         'email_to' => $postData['customer_email'],
                         'email_to_name' => $postData['customer_email'],
-                        'token' => $token_active
+                        'token_reset' => $token_active
                     );
                     $this->Memailqueue->createEmail($dataEmail, 99);
                     $this->success200($flag, $this->lang->line('successfully_register_account'));
@@ -307,6 +308,7 @@ class Customer extends MY_Controller {
             if ($customerId > 0) {
                 $dataUpdate = array(
                     'customer_password' => md5($postData['customer_password']),
+                    'token' => '',
                     'token_reset' => '',
                     'updated_at' => getCurentDateTime()
                 );
@@ -359,11 +361,16 @@ class Customer extends MY_Controller {
             $customer = $this->Mcustomers->getBy(array('token_reset' => $postData['token'], 'customer_status_id' => STATUS_WAITING_ACTIVE));
             if($customer) {
                 $customer = $customer[0];
-                $flag = $this->Mcustomers->save(['customer_status_id' => STATUS_ACTIVED, 'updated_at' => getCurentDateTime()], $customer['id']);
+
+                $this->load->library('Authorization_Token');
+                // generte a token
+                $token = $this->authorization_token->generateToken(array('id' => $customer['id'], 'created_at' => getCurentDateTime()));
+
+                $flag = $this->Mcustomers->save(['token_reset' => '', 'token' => $token, 'customer_status_id' => STATUS_ACTIVED, 'updated_at' => getCurentDateTime()], $customer['id']);
                 if($flag) {
                     if(empty($customer['customer_avatar'])) $customer['customer_avatar'] = base_url(CUSTOMER_PATH.NO_IMAGE);
                     else $customer['customer_avatar'] = base_url(CUSTOMER_PATH.$customer['customer_avatar']);
-                    $customer['token'] = $customer['token_reset'];
+                    $customer['token'] = $token;
                     unset($customer['token_reset'], $customer['customer_password'], $customer['created_at'], $customer['created_by'], $customer['updated_at'],  $customer['updated_by'],  $customer['deleted_at']);
                     $this->success200(array('customer' => $customer));
                 } else {
